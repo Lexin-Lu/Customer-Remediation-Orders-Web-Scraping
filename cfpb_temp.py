bacd899b4d2d0811af77c9de96f4d68dfe978784
Lu <lexin.lu@oliverwyman.com>
2024-11-05 10:25:51 -0500

    Add functions get_total_pages and get_order_links

--- cfpb_temp.py
+++ cfpb_temp.py
@@ -27,23 +27,35 @@ driver = webdriver.Chrome(service=chrome_services, options=chrome_options)
 # Set up date filter and base urls
 base_url = "https://www.consumerfinance.gov"
 enforcement_path = "/enforcement/actions"
-start_date = datetime(2022,1,1)
+start_date = datetime(2022,1,1,0,0,0)
 
 
-# Navigate to the base page
-driver.get(base_url+enforcement_path)
-time.sleep(2)
+
+# Step 1: Get the total page of the website
+def get_total_pages(main_page_link):
+    # Navidate to the main page
+    driver.get(main_page_link)
+    time.sleep(2)
+
+    # Parse the main page
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    page_input = soup.find('input', id = "m-pagination__current-page-0")
+    if page_input and page_input.has_attr("max"):
+        total_pages = int(page_input['max'])
+    else: total_pages = 1
+
+    return total_pages
 
 
-res = []
-
-# Step 1: Scrape order links after 1/1/2022 across all pages
+# Step 2: Scrape order links after 1/1/2022 across all pages
 def get_order_links():
     order_links = []
     page = 1
-    while True:
+    main_page_link = base_url+enforcement_path
+    scrape = True
+    while scrape:
         # Navigate to the page
-        url = f"{base_url}?page={page}"
+        url = f"{base_url+enforcement_path}?page={page}"
         driver.get(url)
         time.sleep(2)
         
@@ -52,20 +64,62 @@ def get_order_links():
 
         # Find all order entries 
         orders = soup.find_all('article', class_ = "o-post-preview")
-        if not orders: break # Stop if hit the end of the page
-        print(orders)
+        if not orders: 
+            break # stop if hit the end of the page
 
         # Scrape all order link
         for order in orders: 
             date_tag = order.find('span', class_ = 'datetime').find("time")
             if date_tag:
                 order_date = date_tag["datetime"]
-                if order_date > "2022-01-01T00:00:00":
+                if order_date <=  start_date:
+                    scrape = False
+                    break # stop if prior to start date
+                else:
                     link_tag = order.find('h3', class_ = "o-post-preview__title").find('a')
                     order_links.append(base_url+link_tag['href'])
         page +=1
-
+        if page > get_total_pages(main_page_link): 
+            break # stop if hit the last page
+        
     return order_links
 
+temp_1104 = get_order_links()
+len(temp_1104)
 
-get_order_links()
+
+# Step 2: Scrape details of each order
+def get_order_details(link):
+    # Navigate to the detial page
+    link = temp_1104[4]
+    driver.get(link)
+    time.sleep(2)
+
+    # Parse the detial page contents
+    detail_soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    # Extract order detials
+    order_detail = {}
+
+    details_items = detail_soup.find_all("div", class_ = "m-related-metadata__item-container")
+
+# Forum
+# Docket number
+# Initial filing date
+# Status
+# Products
+# Name of the institution
+# Civil money penalty
+# Redress amount
+
+
+# Main function
+# res = []
+#def main():
+    # Navigate to the base page
+    # driver.get(base_url+enforcement_path)
+    # time.sleep(2)
+
+
+    
+
